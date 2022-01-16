304393f8d2a978b4a01bde1ae52bd141c4a65c13
Pavel Gladyshev <pavelnicgladyshev@gmail.com>
2022-01-17 01:42:46 +0300

    adding user owner

--- src/main/java/com/gladbros/MangaBookStore/services/ProductService.java
+++ src/main/java/com/gladbros/MangaBookStore/services/ProductService.java
@@ -2,13 +2,16 @@ package com.gladbros.MangaBookStore.services;
 
 import com.gladbros.MangaBookStore.models.Image;
 import com.gladbros.MangaBookStore.models.Product;
+import com.gladbros.MangaBookStore.models.User;
 import com.gladbros.MangaBookStore.repositories.ProductRepository;
+import com.gladbros.MangaBookStore.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.List;
 
 //private String title;
@@ -27,13 +30,15 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     public List <Product> listProducts(String title){
         if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
-    public void saveProducts(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+    public void saveProducts(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        product.setUser(getUserByPrincipal(principal));
         Image image1;
         Image image2;
         Image image3;
@@ -50,10 +55,15 @@ public class ProductService {
             image3 = toImageEntity(file3);
             product.addImageToProduct(image3);
         }
-        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(),product.getAuthor());
+        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(),product.getUser().getEmail());
         Product productFromDb = productRepository.save(product);
         productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
     }
 
     private Image toImageEntity(MultipartFile file) throws IOException {
